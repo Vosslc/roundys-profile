f9823ba4e49185b3161cc86955335049976469df
Roundy <nroundy801@gmail.com>
2020-01-08 15:34:30 -0700

    mapped over proficiencies.js and cleaned up some code

---
+++ src/components/Skills/Proficiencies.js
@@ -0,0 +1,85 @@
+import JS from "./assets/JS.png";
+import CSS3 from "./assets/CSS3.png";
+import HTML5 from "./assets/HTML5_Badge_256.png";
+import React from "./assets/React.png";
+import Nodejs from "./assets/icons8-nodejs-144.png";
+import Redux from "./assets/Redux.svg";
+import Bootstrap from "./assets/Bootstrap.svg";
+import Jest from "./assets/Jest.png";
+import Airbnb from "./assets/Airbnb.svg";
+import Heroku from "./assets/Heroku.svg";
+import Postgresql from "./assets/Postgresql.svg";
+import Sass from "./assets/Sass.svg";
+
+export const Proficiencies = [
+  {
+    name: "Javascript",
+    src: JS,
+    href: "#/us"
+  },
+  {
+    name: "Nodejs",
+    src: Nodejs,
+    href: "#/us"
+  },
+  {
+    name: "React",
+    src: React,
+    href: "#/us"
+  },
+  {
+    name: "Redux",
+    src: Redux,
+    href: "#/us"
+  },
+
+  {
+    name: "HTML5",
+    src: HTML5,
+    href: "#/us"
+  },
+  {
+    name: "CSS3",
+    src: CSS3,
+    href: "#/us"
+  },
+
+  {
+    name: "Sass",
+    src: Sass,
+    href: "#/us"
+  },
+
+  {
+    name: "Bootstrap",
+    src: Bootstrap,
+    href: "#/us"
+  },
+  {
+    name: "Jest",
+    src: Jest,
+    href: "#/us"
+  },
+  {
+    name: "Enzyme",
+    src: Airbnb,
+    href: "#/us"
+  },
+  {
+    name: "Database",
+    src: Heroku,
+    href: "#/us"
+  },
+  {
+    name: "Postgresql",
+    src: Postgresql,
+    href: "#/us"
+  }
+];
+
+// import proficiencies from './projaskdjf';
+// proficiencies.map(function(element) => {
+// return `
+//   //jsx       <img src={element.img} />
+//   //jsx       <span>{element.name}</span>
+// })
